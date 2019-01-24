8f52befe1b886a14086c1e75e4845281d60f756d
noctwolf <noctwolf@gmail.com>
2019-01-24 23:29:34 +0800

    MessageBoxException

---
+++ Vikings.Wpf.Extensions/Windows/MessageBoxException.cs
@@ -0,0 +1,26 @@
+﻿using System.Linq;
+
+namespace System.Windows
+{
+    public class MessageBoxException
+    {
+        public MessageBoxException(string messageBoxText, string caption = null, MessageBoxImage icon = MessageBoxImage.None)
+        {
+            MessageBoxText = messageBoxText;
+            Caption = caption ?? Application.Current.MainWindow.Title;
+            Icon = icon;
+        }
+
+        public string MessageBoxText { get; }
+
+        public string Caption { get; }
+
+        public MessageBoxImage Icon { get; }
+
+        public void Show()
+        {
+            var owner = Application.Current.Windows.OfType<Window>().Single(f => f.IsActive);
+            MessageBox.Show(owner, MessageBoxText, Caption, MessageBoxButton.OK, Icon);
+        }
+    }
+}
