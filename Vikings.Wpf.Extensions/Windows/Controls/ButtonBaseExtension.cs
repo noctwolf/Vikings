7bc6404ccdc882da17c14a218b5549367dcc2c71
noctwolf <noctwolf@gmail.com>
2019-01-26 20:32:46 +0800

    文档注释

--- Vikings.Wpf.Extensions/Windows/Controls/ButtonBaseExtension.cs
+++ Vikings.Wpf.Extensions/Windows/Controls/ButtonBaseExtension.cs
@@ -1,14 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Reflection;
 
 namespace System.Windows.Controls
 {
+    /// <summary>
+    /// ButtonBase 扩展
+    /// </summary>
     public static class ButtonBaseExtension
     {
+        /// <summary>
+        /// 引发 <see cref="Primitives.ButtonBase.Click" /> 路由事件。
+        /// </summary>
+        /// <param name="buttonBase">要引发路由事件的按钮</param>
         public static void OnClick(this Primitives.ButtonBase buttonBase)
         {
             buttonBase.GetType().GetMethod(nameof(OnClick), BindingFlags.Instance | BindingFlags.NonPublic).Invoke(buttonBase, null);
--- Vikings.Wpf.Extensions/Windows/WindowExtension.cs
+++ Vikings.Wpf.Extensions/Windows/WindowExtension.cs
@@ -2,8 +2,20 @@
 
 namespace System.Windows
 {
+    /// <summary>
+    /// Window 扩展
+    /// </summary>
     public static class WindowExtension
     {
+        /// <summary>
+        /// 打开一个窗口，并且仅在新打开的窗口关闭后才返回。
+        /// </summary>
+        /// <param name="window">要显示的窗口</param>
+        /// <param name="owner">窗口的拥有者窗口。如指定窗口的子，会自动找窗口。如指定为空，会使用当前激活窗口</param>
+        /// <returns>
+        /// <see cref="bool" /> 类型的 <see cref="System.Nullable&lt;T&gt;" /> 值， 该值指定活动被接受 (<see langword="true" />) 还是被取消 (<see langword="false" />)。
+        /// 返回值是 <see cref="Window.DialogResult" /> 属性在窗口关闭前具有的值
+        /// </returns>
         public static bool? ShowDialog(this Window window, FrameworkElement owner)
         {
             while (!(owner != null && owner is Window)) owner = owner.Parent as FrameworkElement;
