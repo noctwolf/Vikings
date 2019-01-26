c59b9607575f9fd5f344ef029933365a243f3792
noctwolf <noctwolf@gmail.com>
2019-01-26 20:32:46 +0800

    文档注释

--- Vikings.Wpf.Extensions/Windows/UserCanceledException.cs
+++ Vikings.Wpf.Extensions/Windows/UserCanceledException.cs
@@ -1,5 +1,8 @@
 ﻿namespace System.Windows
 {
+    /// <summary>
+    /// 返回，比如用于在 <see cref="Controls.Primitives.ButtonBase.OnClick" /> 中取消 <see cref="Controls.Primitives.ButtonBase.Command" />Command的执行
+    /// </summary>
     public class UserCanceledException: Exception
     {
         static UserCanceledException()
@@ -7,7 +10,7 @@
             Application.Current.DispatcherUnhandledException += Current_DispatcherUnhandledException;
         }
 
-        private static void Current_DispatcherUnhandledException(object sender, Threading.DispatcherUnhandledExceptionEventArgs e)
+        static void Current_DispatcherUnhandledException(object sender, Threading.DispatcherUnhandledExceptionEventArgs e)
         {
             if (e.Exception is UserCanceledException) e.Handled = true;
         }
