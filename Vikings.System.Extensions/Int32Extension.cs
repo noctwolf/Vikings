2c6705dcb05b5849f0894c7ddf52f0b082f22577
noctwolf <noctwolf@gmail.com>
2019-01-27 16:57:13 +0800

    添加注释

--- Vikings.System.Extensions/Int32Extension.cs
+++ Vikings.System.Extensions/Int32Extension.cs
@@ -1,21 +1,45 @@
 ﻿using System.Collections.Generic;
+using System.Text;
 
 namespace System
 {
+    /// <summary>
+    /// int 扩展
+    /// </summary>
     public static class Int32Extension
     {
+        /// <summary>
+        /// 把长度尽量均匀切分
+        /// </summary>
+        /// <param name="total">要切分的长度</param>
+        /// <param name="count">要切分的份数</param>
+        /// <returns>每一段长度的可枚举集合</returns>
         public static IEnumerable<int> Split(this int total, int count)
         {
-            if (total < 1) throw new ArgumentException("参数值必须大于零", nameof(total));
-            if (count < 1) throw new ArgumentException("参数值必须大于零", nameof(count));
+            if (total <= 0) throw new ArgumentException("参数值必须大于零", nameof(total));
+            if (count <= 0) throw new ArgumentException("参数值必须大于零", nameof(count));
             double split = total * 1d / count;
             for (int i = 0; i < count; i++)
                 yield return Convert.ToInt32(split * (i + 1)) - Convert.ToInt32(split * i);
         }
 
-        public static string ToByteSize(this int value)
+        /// <summary>
+        /// 将数值转换为字符串，该字符串表示以字节，KB，MB或GB表示的大小值表示的数字，具体取决于大小。
+        /// </summary>
+        /// <param name="value">要转换的数值</param>
+        /// <returns>数值字符串</returns>
+        public static string ToByteSize(this int value) => FormatByteSize(value);
+
+        /// <summary>
+        /// 将数值转换为字符串，该字符串表示以字节，KB，MB或GB表示的大小值表示的数字，具体取决于大小。
+        /// </summary>
+        /// <param name="length">要转换的数值</param>
+        /// <returns>数值字符串</returns>
+        public static string FormatByteSize(this long length)
         {
-            return Winapi.Shlwapi.FormatByteSize(value);
+            StringBuilder sb = new StringBuilder(64);
+            Vanara.PInvoke.ShlwApi.StrFormatByteSizeW(length, sb, (uint)sb.Capacity);
+            return sb.ToString();
         }
     }
 }
--- Vikings.System.Extensions/Net/IPAddressExtension.cs
+++ Vikings.System.Extensions/Net/IPAddressExtension.cs
@@ -3,14 +3,37 @@ using System.Net.Sockets;
 
 namespace System.Net
 {
+    /// <summary>
+    /// IPAddress 扩展
+    /// </summary>
     public static class IPAddressExtension
     {
+        /// <summary>
+        /// 转为 <see cref="IPAddress"/>
+        /// </summary>
+        /// <param name="value">要转换的值</param>
+        /// <returns><see cref="IPAddress"/> 的新实例</returns>
         public static IPAddress ToIPAddress(this uint value) => new IPAddress(BitConverter.GetBytes(value).Reverse().ToArray());
 
+        /// <summary>
+        /// 转为 <see cref="IPAddress"/>
+        /// </summary>
+        /// <param name="value">要转换的值</param>
+        /// <returns><see cref="IPAddress"/> 的新实例</returns>
         public static IPAddress ToIPAddress(this int value) => ((uint)value).ToIPAddress();
 
+        /// <summary>
+        /// 转为 <see cref="IPAddress"/>
+        /// </summary>
+        /// <param name="value">要转换的值</param>
+        /// <returns><see cref="IPAddress"/> 的新实例</returns>
         public static IPAddress ToIPAddress(this long value) => ((uint)value).ToIPAddress();
 
+        /// <summary>
+        /// 转为 <see cref="uint"/>
+        /// </summary>
+        /// <param name="value">要转换的值</param>
+        /// <returns><see cref="uint"/> 的新实例</returns>
         public static uint ToUInt32(this IPAddress value)
         {
             if (value.AddressFamily != AddressFamily.InterNetwork)
@@ -18,6 +41,11 @@ namespace System.Net
             return BitConverter.ToUInt32(value.GetAddressBytes().Reverse().ToArray(), 0);
         }
 
+        /// <summary>
+        /// 转为 <see cref="int"/>
+        /// </summary>
+        /// <param name="value">要转换的值</param>
+        /// <returns><see cref="int"/> 的新实例</returns>
         public static int ToInt32(this IPAddress value) => (int)value.ToUInt32();
     }
 }
