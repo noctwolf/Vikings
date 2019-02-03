750274720ac5c395c98ab4994442bbcda4d11d49
noctwolf <noctwolf@gmail.com>
2019-02-04 02:49:25 +0800

    发布

--- Vikings.Base/Properties/AssemblyInfo.cs
+++ Vikings.Base/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // 控制。更改这些特性值可修改
 // 与程序集关联的信息。
 [assembly: AssemblyTitle("Vikings.Base")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Vikings Base")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Vikings")]
 [assembly: AssemblyProduct("Vikings.Base")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
