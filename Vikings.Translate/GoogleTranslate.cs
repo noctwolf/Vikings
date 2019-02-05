cfdfab03dcf9aabba832c45a84616ad5188241a7
noctwolf <noctwolf@gmail.com>
2019-02-05 19:21:17 +0800

    重构GetTK

--- Vikings.Translate/GoogleTranslate.cs
+++ Vikings.Translate/GoogleTranslate.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Net;
 using System.Text;
@@ -34,7 +35,7 @@ namespace Vikings.Translate
         {
             text = text.StripBidiControlCharacter();
             var cc = new CookieContainer();
-            var tk = JScript.GetTK(text, GetTKK(BaseUrl, cc));
+            var tk = GetTK(text, GetTKK(BaseUrl, cc));
             string googleTransUrl = BaseUrl + "translate_a/single?client=webapp&sl=" + fromLanguage + "&tl=" + toLanguage + "&hl=en&dt=at&dt=bd&dt=ex&dt=ld&dt=md&dt=qca&dt=rw&dt=rm&dt=ss&dt=t&ie=UTF-8&oe=UTF-8&otf=1&ssel=0&tsel=0&kc=1&tk=" + tk + "&q=" + WebUtility.UrlEncode(text);
             var ResultHtml = GetResultHtml(googleTransUrl, cc, BaseUrl);
             dynamic TempResult = Newtonsoft.Json.JsonConvert.DeserializeObject(ResultHtml);
@@ -73,6 +74,67 @@ namespace Vikings.Translate
                     return html;
                 }
             }
+        }
+
+        /// <summary>
+        /// 获取字符串的tk
+        /// </summary>
+        /// <param name="text">要计算的字符串</param>
+        /// <param name="tkk">哈希参数</param>
+        /// <returns>计算好的tk</returns>
+        public static string GetTK(string text, string tkk)
+        {
+            if (string.IsNullOrEmpty(text)) throw new ArgumentNullException(nameof(text));
+            if (string.IsNullOrEmpty(tkk)) throw new ArgumentNullException(nameof(tkk));
+            var tkkSplit = tkk.Split('.');
+            if (!(tkkSplit.Length == 2 && int.TryParse(tkkSplit[0], out _) && int.TryParse(tkkSplit[1], out _)))
+                throw new ArgumentException(nameof(tkk));
+            var list = new List<int>();
+            for (int i = 0; i < text.Length; i++)
+            {
+                int charCode = text[i];
+                if (charCode < 0x80) list.Add(charCode);
+                else
+                {
+                    if (charCode < 0x800)
+                        list.Add(charCode >> 6 | 0xC0);
+                    else
+                    {
+                        if ((charCode & 0xFC00) == 0xD800 && i + 1 < text.Length && (text[i + 1] & 0xFC00) == 0xDC00)
+                        {
+                            charCode = 0x10000 + ((charCode & 0x3FF) << 10) + (text[++i] & 0x3FF);
+                            list.Add(charCode >> 18 | 0xF0);
+                            list.Add(charCode >> 12 & 0x3F | 0x80);
+                        }
+                        else
+                            list.Add(charCode >> 12 | 0xE0);
+                        list.Add(charCode >> 6 & 0x3F | 0x80);
+                    }
+                    list.Add(charCode & 0x3F | 0x80);
+                }
+            }
+            int tkk0 = int.Parse(tkkSplit[0]);
+            var r = tkk0;
+            for (int i = 0; i < list.Count; i++)
+            {
+                r += list[i];
+                r = HashTK(r, "+-a^+6");
+            }
+            r = HashTK(r, "+-3^+b+-f");
+            r ^= int.Parse(tkkSplit[1]);
+            var result = (uint)r % 1000000;
+            return $"{result}.{result ^ tkk0}";
+        }
+
+        static int HashTK(int r, string expression)
+        {
+            for (int i = 0; i < expression.Length - 2; i += 3)
+            {
+                var bit = Convert.ToInt32(expression[i + 2].ToString(), 16);
+                var offset = expression[i + 1] == '+' ? (int)((uint)r >> bit) : r << bit;
+                r = expression[i] == '+' ? r + offset : r ^ offset;
+            }
+            return r;
         }
     }
 }
--- Vikings.Translate/JScript.cs
+++ Vikings.Translate/JScript.cs
@@ -1,8 +1,6 @@
 ﻿using Microsoft.JScript;
 using System.CodeDom.Compiler;
 using System.Reflection;
-using System;
-using System.Collections.Generic;
 
 namespace Vikings.Translate
 {
@@ -18,58 +16,6 @@ namespace Vikings.Translate
         }
 
         public static string GetTK(string text, string tkk) => infoGetTK.Invoke(null, new object[] { text, tkk }).ToString();
-
-        public static string TK(string a, string TKK)
-        {
-            if (string.IsNullOrEmpty(a) || string.IsNullOrEmpty(TKK)) throw new ArgumentNullException();
-            var e = TKK.Split('.');
-            if (!(e.Length == 2 && int.TryParse(e[0], out _) && int.TryParse(e[1], out _))) throw new ArgumentNullException();
-            var g = new List<int>();
-            for (int f = 0; f < a.Length; f++)
-            {
-                int c = a[f];
-                if (c < 128) g.Add(c);
-                else
-                {
-                    if (c < 2048)
-                        g.Add(c >> 6 | 192);
-                    else
-                    {
-                        if ((c & 64512) == 55296 && f + 1 < a.Length && (a[f + 1] & 64512) == 56320)
-                        {
-                            c = 65536 + ((c & 1023) << 10) + (a[++f] & 1023);
-                            g.Add(c >> 18 | 240);
-                            g.Add(c >> 12 & 63 | 128);
-                        }
-                        else
-                            g.Add(c >> 12 | 224);
-                        g.Add(c >> 6 & 63 | 128);
-                    }
-                    g.Add(c & 63 | 128);
-                }
-            }
-            var r = int.Parse(e[0]);
-            for (int d = 0; d < g.Count; d++)
-            {
-                r += g[d];
-                r = B(r, "+-a^+6");
-            }
-            r = B(r, "+-3^+b+-f");
-            r ^= int.Parse(e[1]);
-            var v = (uint)r % 1000000;
-            return v.ToString() + "." + (v ^ int.Parse(e[0]));
-        }
-
-        private static int B(int a, string expression)
-        {
-            for (int i = 0; i < expression.Length - 2; i += 3)
-            {
-                var bit = System.Convert.ToInt32(expression[i + 2].ToString(), 16);
-                var offset = expression[i + 1] == '+' ? (int)((uint)a >> bit) : a << bit;
-                a = expression[i] == '+' ? a + offset : a ^ offset;
-            }
-            return a;
-        }
     }
 }
 
