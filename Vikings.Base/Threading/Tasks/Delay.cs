5c6d6811a823146c1fb89d4467461caf042ccf12
noctwolf <noctwolf@gmail.com>
2019-06-01 18:03:24 +0800

    重构为防抖和节流

--- Vikings.Base/Threading/Tasks/Delay.cs
+++ Vikings.Base/Threading/Tasks/Delay.cs
@@ -11,46 +11,60 @@ namespace Vikings.Threading.Tasks
     /// </summary>
     public static class Delay
     {
-        [ThreadStatic] static Dictionary<string, Caller> callerDict;
+        private class Caller
+        {
+            protected internal CancellationTokenSource CancellationTokenSource;
+            protected internal DateTime LastTime;
+        }
 
-        static void ThreadInit() => callerDict = callerDict ?? new Dictionary<string, Caller>();
+        [ThreadStatic]
+        private static Dictionary<string, Caller> callerDict;
+
+        private static void ThreadInit() => callerDict = callerDict ?? new Dictionary<string, Caller>();
 
         private static async Task Async(Action action, int milliseconds, string name, bool isInterval = false)
         {
             ThreadInit();
-            if (!callerDict.ContainsKey(name)) callerDict[name] = new Caller { name = name };
+            if (!callerDict.ContainsKey(name)) callerDict[name] = new Caller();
             var caller = callerDict[name];
-            caller.cancellationTokenSource?.Cancel();
-            caller.cancellationTokenSource = new CancellationTokenSource();
-            var ct = caller.cancellationTokenSource.Token;
-            if (isInterval) milliseconds -= (int)(DateTime.Now - caller.lastTime).TotalMilliseconds;
+            caller.CancellationTokenSource?.Cancel();
+            caller.CancellationTokenSource = new CancellationTokenSource();
+            var ct = caller.CancellationTokenSource.Token;
+            if (isInterval) milliseconds -= (int)(DateTime.Now - caller.LastTime).TotalMilliseconds;
             if (milliseconds > 0)
             {
                 var task = Task.Delay(milliseconds, ct);
                 await task;
                 if (ct.IsCancellationRequested) throw new TaskCanceledException(task);//处理取消时已经完成延迟任务
             }
-            caller.lastTime = DateTime.Now;
+            caller.LastTime = DateTime.Now;
             action?.Invoke();
         }
 
         /// <summary>
+        /// 防抖
+        /// </summary>
+        public static Task DebounceAsync(Action action = null, int milliseconds = 500, [CallerFilePath]string name = "")
+            => Async(action, milliseconds, name);
+
+        /// <summary>
+        /// 节流
+        /// </summary>
+        public static Task ThrottleAsync(Action action = null, int milliseconds = 1000, [CallerFilePath]string name = "")
+            => Async(action, milliseconds, name, true);
+
+        /// <summary>
         /// 固定时间间隔
         /// </summary>
+        [Obsolete("Use ThrottleAsync")]
         public static Task IntervalAsync(Action action = null, int milliseconds = 1000, [CallerFilePath]string name = "")
-            => Async(action, milliseconds, name, true);
+            => ThrottleAsync(action, milliseconds, name);
 
         /// <summary>
         /// 重置延迟时间
         /// </summary>
+        [Obsolete("Use DebounceAsync")]
         public static Task ResetAsync(Action action = null, int milliseconds = 500, [CallerFilePath]string name = "")
-            => Async(action, milliseconds, name);
-
-        class Caller
-        {
-            protected internal string name;
-            protected internal CancellationTokenSource cancellationTokenSource;
-            protected internal DateTime lastTime;
-        }
+            => DebounceAsync(action, milliseconds, name);
     }
 }
