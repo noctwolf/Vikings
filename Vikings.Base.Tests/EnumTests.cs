4bc36b594a47f7882cc78f49526622003c4687c8
noctwolf <noctwolf@gmail.com>
2019-07-23 00:09:57 +0800

    Enum.ToDictionary<Test>()

---
+++ Vikings.Base.Tests/EnumTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Vikings.Base.Tests
+{
+    [TestClass]
+    public class EnumTests
+    {
+        [TestMethod]
+        public void ToDictionaryTest()
+        {
+            var dict = Enum.ToDictionary<Test>();
+            dict.ToList().ForEach(f => Console.WriteLine($"{f.Key}={f.Value}"));
+            Assert.IsTrue(dict.Any(f => f.Key == Test.A && f.Value == "AAA"));
+            Assert.IsTrue(dict.Any(f => f.Key == Test.Ab && f.Value == "BBB"));
+            Assert.IsTrue(dict.Any(f => f.Key == Test.Abc && f.Value == "Abc"));
+        }
+    }
+
+    public enum Test
+    {
+        [System.ComponentModel.Description("AAA")]
+        A,
+        [System.ComponentModel.Description("BBB")]
+        Ab,
+        //[System.ComponentModel.Description("CCC")]
+        Abc
+    }
+}
---
+++ Vikings.Base/Enum.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Reflection;
+
+namespace Vikings
+{
+    /// <summary>
+    /// enum 扩展
+    /// </summary>
+    public static class Enum
+    {
+        /// <summary>
+        /// 获取<see langword="enum"/>包含描述的Dictionary
+        /// </summary>
+        /// <typeparam name="TKey"><see langword="enum"/>类型</typeparam>
+        /// <returns>包含描述的Dictionary</returns>
+        public static Dictionary<TKey, string> ToDictionary<TKey>() where TKey : System.Enum =>
+            typeof(TKey).GetEnumValues().OfType<TKey>().ToDictionary(f => f, f =>
+                typeof(TKey).GetMember(f.ToString())[0].GetCustomAttribute<DescriptionAttribute>()?.Description ?? f.ToString());
+    }
+}
