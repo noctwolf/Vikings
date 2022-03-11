99eebc58976f6857ba6f86ca529fd2929beb31e0
noctwolf <noctwolf@gmail.com>
2022-03-11 11:56:48 +0800

    BitmapImageExtension

---
+++ Vikings.Wpf.Extensions/Windows/Media/Imaging/BitmapImageExtension.cs
@@ -0,0 +1,34 @@
+﻿using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+
+namespace System.Windows.Media.Imaging
+{
+    /// <summary>
+    /// BitmapImage 扩展
+    /// </summary>
+    public static class BitmapImageExtension
+    {
+        /// <summary>
+        /// <see cref="Image"/>转换为<see cref="BitmapImage"/>
+        /// </summary>
+        /// <param name="value">要转换的图片</param>
+        /// <param name="imageFormat">为空时，使用<see cref="ImageFormat.Png"/></param>
+        /// <returns></returns>
+        public static BitmapImage ToBitmapImage(this Image value, ImageFormat imageFormat = null)
+        {
+            using (var memoryStream = new MemoryStream())
+            {
+                value.Save(memoryStream, ImageFormat.Png);
+                memoryStream.Position = 0;
+                var bitmapImage = new BitmapImage();
+                bitmapImage.BeginInit();
+                bitmapImage.CacheOption = BitmapCacheOption.OnLoad;
+                bitmapImage.StreamSource = memoryStream;
+                bitmapImage.EndInit();
+                bitmapImage.Freeze();
+                return bitmapImage;
+            }
+        }
+    }
+}
